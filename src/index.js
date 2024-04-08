691f1d6266ecca1202f1cc5317156550ff95e267
Ken Richlin <krichlin@gmail.com>
2024-04-08 13:36:11 -0400

    challenge 1 & 2 working

--- src/index.js
+++ src/index.js
@@ -1,71 +1,86 @@
 console.log('%c HI', 'color: firebrick')
 
-document.addEventListener('DOMContentLoaded', function () {
+fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((resp) => resp.json())
+    .then((data) => renderImages(data))
 
-    loadImages();
-    loadBreedOptions();
+function renderImages(objStoringUrls) {
 
-});
+    urlArr = objStoringUrls.message
 
-function loadImages() {
-    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-    fetch(imgUrl)
-    .then(res => res.json())
-    .then(results => {
-        results.message.forEach(image => addImage(image))
-    });
+    const imgContainer = document.querySelector('#dog-image-container')
+    
+    console.log(objStoringUrls)
+    
+    urlArr.forEach((url) => {
+        const img = document.createElement('img')
+        img.src = url
+        imgContainer.appendChild(img)
+    })
 }
 
-function addImage(dogPicUrl) {
+fetch("https://dog.ceo/api/breeds/list/all")
+    .then((resp) => resp.json())
+    .then((data) => renderBreedNames(data))
 
-    let container = document.querySelector('#dog-image-container');
+function renderBreedNames(objStoringBreeds) {
+    breedObj = objStoringBreeds.message
+    console.log(Object.keys(breedObj))
+    breedArr = Object.keys(breedObj)
 
-    let newImageEl = document.createElement('img');
-    newImageEl.src = dogPicUrl;
+    const breedContainer = document.querySelector('#dog-breeds')
 
-    container.appendChild(newImageEl);
-}
-
-let breeds = [];
-
-function loadBreedOptions() {
-
-    const breedUrl = "https://dog.ceo/api/breeds/list/all";
-    console.log(breedUrl);
-
-    fetch(breedUrl)
-    .then(res => res.json())
-    .then(results => {
-        breeds = document.getElementsByTagName("ul");
-        updateBreedList(breeds);
-        addBreedSelectListener();
+    breedArr.forEach((breed) => {
+        const li = document.createElement('li')
+        li.textContent = breed
+        breedContainer.appendChild(li)
     })
 }
 
 
+// document.addEventListener('DOMContentLoaded', function () {
 
+//     loadImages();
+//     loadBreedOptions();
 
+// });
 
+// function loadImages() {
 
+//     const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+//     fetch(imgUrl)
+//     .then(res => res.json())
+//     .then(results => {
+//         results.message.forEach(image => addImage(image))
 
+//     });
+// }
 
+// function addImage(dogPicUrl) {
 
+//     let container = document.querySelector('#dog-image-container');
 
+//     let newImageEl = document.createElement('img');
+//     newImageEl.src = dogPicUrl;
 
+//     container.appendChild(newImageEl);
+// }
 
+// let breeds = [];
 
+// function loadBreedOptions() {
 
+//     const breedUrl = "https://dog.ceo/api/breeds/list/all";
+//     console.log(breedUrl);
 
-
-
-
-
-
-
-
-
-
-
+//     fetch(breedUrl)
+//     .then(res => res.json())
+//     .then(results => {
+//         breeds = document.getElementsByTagName("ul");
+//         updateBreedList(breeds);
+//         addBreedSelectListener();
+//     })
+// }
 
 
 // const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
